1af902a67513e1bdbec07911d39b3a47f749a574
Luciano Miguel <luciano_limamiguel@hotmail.com>
2018-04-03 22:34:25 -0300

    Evento pegando foco

---
+++ ExMySql/teste.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExMySql
+{
+    public partial class FrmTeste : Form
+    {
+        public FrmTeste()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("TEste");
+            textBox1.Clear();
+        }
+
+        private void radioButton1_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ola");
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("CheckedChanged");
+            Form1 f = new Form1();
+            f.Show();
+        }
+    }
+}
---
+++ teste/teste.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teste
+{
+    public partial class FrmTeste : Form
+    {
+        public FrmTeste()
+        {
+            InitializeComponent();
+        }
+
+        private void teste_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
